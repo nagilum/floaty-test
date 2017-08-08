4465af18602fe57fb3af2e9483f46d694bad289d
Stian Hanger <pdnagilum@gmail.com>
2017-08-08 15:39:01 +0200

    Changes

---
+++ source/js/app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var viewUser = function (ctx) {
+    console.log(ctx);
+};
+
+var editUser = function (ctx) {
+    console.log(ctx);
+};
+
+var checkLoggedIn = function (ctx) {
+    throw new Error('BLARGH');
+};
+
+jk(() => {
+    floaty.template('frontpage', 'template#frontpage');
+    floaty.template('about', 'template#about');
+
+    floaty.route('/', () => { floaty.render('frontpage', 'article'); });
+    floaty.route('/about', () => { floaty.render('about', 'article'); });
+
+    floaty.route('/user/:id', viewUser);
+    floaty.route('/user/:id/edit', checkLoggedIn, editUser);
+
+    floaty.notFound((ctx) => { alert('PANIC'); });
+
+    floaty.init({
+        continueOnExceptions: true
+    });
+});
